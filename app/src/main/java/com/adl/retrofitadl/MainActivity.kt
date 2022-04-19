8ac106daba117473166d9b32103b7d5affc65c38
arvsyam <arivasyam31@gmail.com>
2022-04-19 13:19:14 +0700

    Add Expandable list

--- app/src/main/java/com/adl/retrofitadl/MainActivity.kt
+++ app/src/main/java/com/adl/retrofitadl/MainActivity.kt
@@ -13,14 +13,21 @@ import com.adl.retrofitadl.model.OMDBResponse
 import com.adl.retrofitadl.model.SearchItem
 import com.adl.retrofitadl.model.SearchResponse
 import com.adl.retrofitadl.services.RetrofitConfig
+import com.panaceasoft.pskotlinmaterial.adapter.feature.list.education.FeatureListEducationCategoryList1Adapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_template.*
 import kotlinx.android.synthetic.main.movie_holder.*
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
-import javax.security.auth.callback.Callback
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listDataHeader: MutableList<String>
+    private lateinit var listDataChild: HashMap<String, List<SearchItem>>
+    private lateinit var featureListEducationCategoryList1Adapter: FeatureListEducationCategoryList1Adapter
+
+
     var lstMovie = ArrayList<SearchItem?>()
     lateinit var movieAdapter : MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,22 +39,17 @@ class MainActivity : AppCompatActivity() {
         initToolbar()
 //        btn_search.setOnClickListener ({
             RetrofitConfig().getService()
-                .searchMovie(et_search.text.toString())
-                .enqueue(object : retrofit2.Callback<SearchResponse>{
+                .searchMovie("titanic")
+                .enqueue(object : Callback<SearchResponse>{
                     override fun onResponse(
                         call: Call<SearchResponse>,
                         response: Response<SearchResponse>
                     ) {
-
-
-                        lstMovie = ArrayList(response.body()?.search!!)
-                        Log.d("list size","${lstMovie.size}")
-
-
+                      dataGenerate(response.body()?.search as List<SearchItem>)
                     }
 
                     override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                        Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity,t.localizedMessage,Toast.LENGTH_LONG).show()
                     }
 
                 })
@@ -83,6 +85,19 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Log.e("TEAMPS", "Error in set display home as up enabled.")
         }
+
+    }
+
+    fun dataGenerate ( list :List<SearchItem>){
+        listDataHeader = ArrayList()
+        listDataChild = HashMap()
+
+        listDataHeader.add("Result Movie")
+       listDataChild["Result Movie"] = list
+
+        featureListEducationCategoryList1Adapter = FeatureListEducationCategoryList1Adapter(this, listDataHeader, listDataChild)
+
+        lvExp.setAdapter(featureListEducationCategoryList1Adapter)
 
     }
 
