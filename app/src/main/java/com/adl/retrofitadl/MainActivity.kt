920398e0d04f0ad935b492ab95966008a238d5da
arvsyam <arivasyam31@gmail.com>
2022-04-19 12:52:22 +0700

    Add Toolbar

--- app/src/main/java/com/adl/retrofitadl/MainActivity.kt
+++ app/src/main/java/com/adl/retrofitadl/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.adl.retrofitadl
 
 import android.app.PendingIntent.getService
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.adl.retrofitadl.adapter.MovieAdapter
 import com.adl.retrofitadl.model.OMDBResponse
@@ -12,6 +14,7 @@ import com.adl.retrofitadl.model.SearchItem
 import com.adl.retrofitadl.model.SearchResponse
 import com.adl.retrofitadl.services.RetrofitConfig
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_template.*
 import kotlinx.android.synthetic.main.movie_holder.*
 import retrofit2.Call
 import retrofit2.Response
@@ -22,10 +25,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var movieAdapter : MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_template)
 
 
-        btn_search.setOnClickListener ({
+
+        initToolbar()
+//        btn_search.setOnClickListener ({
             RetrofitConfig().getService()
                 .searchMovie(et_search.text.toString())
                 .enqueue(object : retrofit2.Callback<SearchResponse>{
@@ -46,7 +51,39 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 })
-        })
+//        })
+    }
+
+    fun initToolbar() {
+
+        toolbar.setNavigationIcon(R.drawable.baseline_menu_black_24)
+
+        if (toolbar.navigationIcon != null) {
+            toolbar.navigationIcon?.setColorFilter(ContextCompat.getColor(this, R.color.md_white_1000), PorterDuff.Mode.SRC_ATOP)
+        }
+
+        toolbar.title = "Expandable List"
+
+        try {
+            toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.md_white_1000))
+        } catch (e: Exception) {
+            Log.e("TEAMPS", "Can't set color.")
+        }
+
+        try {
+            setSupportActionBar(toolbar)
+        } catch (e: Exception) {
+            Log.e("TEAMPS", "Error in set support action bar.")
+        }
+
+        try {
+            if (supportActionBar != null) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            }
+        } catch (e: Exception) {
+            Log.e("TEAMPS", "Error in set display home as up enabled.")
+        }
+
     }
 
 
